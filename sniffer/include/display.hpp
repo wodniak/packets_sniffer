1e6a24f51c750a938a2959a92a51eca4505cb12d
gwozniak <wozgrz96@gmail.com>
2019-11-17 22:02:35 +0100

    display class (not functional)

---
+++ sniffer/include/display.hpp
@@ -0,0 +1,23 @@
+#ifndef DISPLAY_HPP
+#define DISPLAY_HPP
+
+#include <iostream>
+
+#define CONSOLE_CLEAN std::system("clear")
+
+namespace gw
+{
+
+class Display
+{
+public:
+    Display();
+    void refresh();
+
+private:
+    
+};
+
+} //namespace gw
+
+#endif //DISPLAY_HP
---
+++ sniffer/src/display.cpp
@@ -0,0 +1,18 @@
+#include "display.hpp"
+
+namespace gw
+{
+
+Display::Display()
+{
+
+}
+
+
+void Display::refresh()
+{
+    CONSOLE_CLEAN;
+}
+
+
+} //namespace gw
